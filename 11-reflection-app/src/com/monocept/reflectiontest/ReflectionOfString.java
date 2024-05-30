b972efa265cbac738756f5b87af74d01977df3e3
valletivarish <varishvaleti52@gmail.com>
2024-05-30 13:28:33 +0530

    Reflection of string

---
+++ 11-reflection-app/src/com/monocept/reflectiontest/ReflectionOfString.java
@@ -0,0 +1,30 @@
+package com.monocept.reflectiontest;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class ReflectionOfString {
+
+	public static void main(String[] args) {
+		try {
+			Class stringClass =Class.forName("java.lang.String");
+			System.out.println(stringClass.toString());
+			
+			Method methods[]=stringClass.getMethods();
+			
+			Constructor constructors[]=stringClass.getConstructors();
+			
+			for(Constructor constructor : constructors) {
+				System.out.println(constructor.toString() + "\t" + constructor.getParameterCount());
+			}
+			
+//			for(Method method:methods) {
+//				System.out.println(method.getName());
+//			}
+		} catch (ClassNotFoundException e) {
+		
+			e.printStackTrace();
+		}
+	}
+
+}
